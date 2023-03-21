9a2576e914bfa51cf61f684e1cc73abd00d89d9f
Garrett Johnson <garrett.kjohnson@gmail.com>
2023-03-21 11:26:46 +0900

    Add Hosmer support for curl

--- surface-flow/galacticCurl.js
+++ surface-flow/galacticCurl.js
@@ -13,8 +13,22 @@ import { CurlGenerator } from '../common/CurlGenerator.js';
 
 const POINT_COUNT = 1000;
 const SEGMENTS_COUNT = 2000;
-const SPEED = 0.005 * 120;
 ( async () => {
+
+	let speed = 0.005 * 120;
+	let url = 'https://raw.githubusercontent.com/gkjohnson/3d-demo-data/main/models/threedscans/Crab.glb';
+	let scale = 0.05;
+	const curlGenerator = new CurlGenerator();
+	curlGenerator.scale = 1.5;
+
+	if ( window.location.hash.includes( 'Hosmer' ) ) {
+
+		url = 'https://raw.githubusercontent.com/gkjohnson/3d-demo-data/main/models/threedscans/Hosmer.glb';
+		curlGenerator.scale = 9 * 0.15;
+		speed = 0.05 * 120 * 0.15;
+		scale = 0.05 * 0.15;
+
+	}
 
 	const app = new App();
 	app.init( document.body );
@@ -28,9 +42,9 @@ const SPEED = 0.005 * 120;
 	controls.autoRotate = true;
 	controls.autoRotateSpeed = 0.1;
 
-	const gltf = await new GLTFLoader().setMeshoptDecoder( MeshoptDecoder ).loadAsync( 'https://raw.githubusercontent.com/gkjohnson/3d-demo-data/main/models/threedscans/Crab.glb' );
+	const gltf = await new GLTFLoader().setMeshoptDecoder( MeshoptDecoder ).loadAsync( url );
 	const mesh = gltf.scene.children[ 0 ];
-	mesh.geometry.scale( 0.05, 0.05, 0.05 );
+	mesh.geometry.scale( scale, scale, scale );
 	mesh.geometry.rotateX( - Math.PI / 2 );
 	mesh.geometry.center();
 
@@ -84,8 +98,6 @@ const SPEED = 0.005 * 120;
 
 	const normal = new Vector3();
 	const temp = new Vector3();
-	const curlGenerator = new CurlGenerator();
-	curlGenerator.scale = 1.5;
 	app.toggleLoading();
 	app.update = delta => {
 
@@ -98,7 +110,7 @@ const SPEED = 0.005 * 120;
 			curlGenerator.sample3d( ...surfacePoint, temp );
 
 			const dir = ( i % 2 === 0 ) ? - 1 : 1;
-			temp.normalize().multiplyScalar( SPEED * delta * dir );
+			temp.normalize().multiplyScalar( speed * delta * dir );
 			surf.movePoint( surfacePoint, temp, surfacePoint, direction, normal );
 
 			temp.copy( surfacePoint );
